e10a976b34fe158a9429c0d0aead5958a5a2356c
mosqueradvd <3090228-mosqueradvd@users.noreply.replit.com>
2024-01-17 11:37:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+from bardapi import Bard
+import os
+
+# Configuración de la clave de API para Bard
+os.environ[
+    '_BARD_API_KEY'] = "bAgr56qeQQL0vzEPkRoZsi9Xmg-nN4YK732XAYkSQQZMsHsAkScGc9ZB8WaLVW25RXBp-g."
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get_response", methods=["GET"])
+def get_response():
+    input_text = request.args.get("input_text")
+
+    if input_text:
+        question = f"Cuál es la tienda con el precio mas bajo para el: {input_text} en USA? y dime en qué link puedo ver esa informacion, no es necesario que me expliques nada más."
+        respuesta = Bard().get_answer(question)['content']
+    else:
+        respuesta = ""
+
+    return jsonify({"contenedor": respuesta})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
